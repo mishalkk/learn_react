abee4a72e01db3c40bd21f16b415e8a8c4f1724e
mishalkk3 <mishal.kk@simelabs.com>
2023-01-09 15:21:47 +0530

    conditionals

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 
 function App() {
   const age = 17;
+  const isGreen= true;
 
   return (
     <div className="App">
       {age >= 18 ? <h1>Over AGED</h1> : <h1>Under Aged</h1>}
+      <h1 style={{color: isGreen ? "green" : "red"}}>This Has Color</h1>
+
+      {isGreen && <button>Go Green</button>}
     </div>
   );
 }
