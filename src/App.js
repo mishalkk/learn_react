e43875fb1e18af65c5a96bd41318e33af63e923c
mishalkk3 <mishal.kk@simelabs.com>
2023-01-09 15:07:43 +0530

    ternary operator

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 
 function App() {
+  const age = 17;
 
   return (
     <div className="App">
-      <div>
-        <h1 className='name'>Pedro</h1>
-      </div>
+      {age >= 18 ? <h1>Over AGED</h1> : <h1>Under Aged</h1>}
     </div>
-  )
+  );
 }
+
+export default App;
