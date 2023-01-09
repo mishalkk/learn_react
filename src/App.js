3fe7072e1ae7b0ecd84ce36cf5806b064e17f3b3
mishalkk3 <mishal.kk@simelabs.com>
2023-01-09 11:43:19 +0530

    variables

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
 
 function App() {
+  // const name = "Pedro";
+  // const name = <h1>Pedro</h1>
+  // const age = <h2>25</h2>
+  // const email = <h2>Pedro@gmail.com</h2>
+
   return (
     <div className="App">
-      Hello World
+      {/* <h1>{name}</h1>
+      <h1>{name}</h1>
+      <h1>{name}</h1> */}
+      <div>
+        {/* {name}
+        {age}
+        {email} */}
+        
+      </div>
     </div>
   );
 }
 
+const User = () => {
+  return (
+    <div></div>
+  );
+}
 export default App;
