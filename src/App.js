289ea1c938bdbfa6249e628535d82f892a05f740
mishalkk3 <mishal.kk@simelabs.com>
2023-01-09 14:55:02 +0530

    Module 3

--- src/App.js
+++ src/App.js
@@ -5,20 +5,8 @@ function App() {
   return (
     <div className="App">
       <div>
-        <User name="Pedro" age={25} mail="pedro@gmail.com" />
-        <User name="Mishal" age={17} mail="mishal@gmail.com" />
+        <h1 className='name'>Pedro</h1>
       </div>
     </div>
-  );
+  )
 }
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h1>{props.age}</h1>
-      <h1>{props.mail}</h1>
-    </div>
-  );
-}
-export default App;
