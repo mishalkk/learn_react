c0f6c9f56714c98ee2eabdc489d87f0315772580
mishalkk3 <mishal.kk@simelabs.com>
2023-01-09 11:45:18 +0530

    BAsic component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
 
 function App() {
-  // const name = "Pedro";
-  // const name = <h1>Pedro</h1>
-  // const age = <h2>25</h2>
-  // const email = <h2>Pedro@gmail.com</h2>
 
   return (
     <div className="App">
-      {/* <h1>{name}</h1>
-      <h1>{name}</h1>
-      <h1>{name}</h1> */}
       <div>
-        {/* {name}
-        {age}
-        {email} */}
-        
+        <User />
       </div>
     </div>
   );
@@ -23,7 +13,11 @@ function App() {
 
 const User = () => {
   return (
-    <div></div>
+    <div>
+      <h1>Pedro</h1>
+      <h1>25</h1>
+      <h1>pedro@gmail.com</h1>
+    </div>
   );
 }
 export default App;
