3b637812b39a56004df784c33204c28304a1a935
mishalkk3 <mishal.kk@simelabs.com>
2023-01-30 16:07:15 +0530

    Module 4

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import './App.css';
-import { User, Planet } from './User';
 
 function App() {
-  const users = [
-    {name:"Mugen", age:21},
-    {name:"Kusho", age:25},
-    {name:"Pedro", age:30}
-  ];
-
-  const planets = [
-    {name:"Mars", isGasPlanet: false},
-    {name:"Earth", isGasPlanet: false},
-    {name:"Jupiter", isGasPlanet: true},
-    {name:"Venus", isGasPlanet: false},
-    {name:"Neptune", isGasPlanet: true},
-    {name:"Uranus", isGasPlanet: true},
-  ]
+  const age = 0;
 
   return (
-    <div className="App">
-      {users.map((user, key) => {
-        return (
-          <User name={user.name} age={user.age} />
-        );
-      })}
-      {planets.map((planet, key) => {
-        return (
-          <Planet name={planet.name} isGasPlanet={planet.isGasPlanet} />
-        );
-      })}
-    </div>
+    <div className="App">{age}</div>
   );
 }
 
--- src/User.js
+++ src/User.js
@@ -1,15 +1,1 @@
-export const User = (props) => {
-    return (
-      <div>
-        {props.name} {props.age}
-      </div>
-    )
-};
-
-
-export const Planet = (props) => {
-    if (props.isGasPlanet) return (
-        <div>{props.name}</div>
-    )
-}
 
