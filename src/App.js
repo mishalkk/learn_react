b0bc63ed609161c37017a2292228199354af09b3
mishalkk3 <mishal.kk@simelabs.com>
2023-02-01 15:19:57 +0530

    Module5

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <button onClick={() => {setCount(count+1)}}>Increase</button>
-      <button onClick={() => {setCount(count-1)}}>Decrease</button>
-      <button onClick={() => {setCount(0)}}>Set to Zero</button>
-      <h1>{count}</h1>
+      Hello World
     </div>
   );
 }
