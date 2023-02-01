a913431f16b3cc000d0f36ba93632fc2df716a89
mishalkk3 <mishal.kk@simelabs.com>
2023-02-01 14:26:03 +0530

    reflecting inputs

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [age, setAge] = useState(0);
+  const [inputValue, setInputValue] = useState("");
 
-  const increaseAge = () => {
-    setAge(age+1);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
   };
 
   return (
     <div className="App">
-      {age} <button onClick={increaseAge}>Incr Age</button>
+      <input type="text" onChange={handleInputChange} />
+      {inputValue}
     </div>
   );
 }
