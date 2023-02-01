29479dae1b6af8717655421e770a69a9618ddef4
mishalkk3 <mishal.kk@simelabs.com>
2023-02-01 18:13:57 +0530

    todo app phase 1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,47 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleChange = (event) => {
+      setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    setTodoList([...todoList, newTask]);
+  }
+
+  const deleteTask = (taskName) => {
+    // const newTodoList = todoList.filter((task) => {
+    //   if (task === taskName) {
+    //     return false;
+    //   } else {
+    //     return true;
+    //   }
+    // })
+
+    const newTodoList =todoList.filter((task) => task !== taskName);
+
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="App">
-      Hello World
+      <div className='addTask'>
+        <input onChange={handleChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div className='list'>
+        {todoList.map((task) => {
+          return (
+            <div>
+              <h1>{task}</h1>
+              <button onClick={() => deleteTask(task)}>X</button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
