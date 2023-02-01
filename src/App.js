8fd954c421526db2e72f28d5de06dd0aaab13c45
mishalkk3 <mishal.kk@simelabs.com>
2023-02-01 15:13:54 +0530

    counter app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,14 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [inputValue, setInputValue] = useState("");
-  const [showText, setShowText] = useState(true);
-
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <input type="text" onChange={handleInputChange} />
-      <button onClick={() => {setShowText(!showText)}}>show/hide</button>
-      {showText && <h1>{inputValue}</h1>}
+      <button onClick={() => {setCount(count+1)}}>Increase</button>
+      <button onClick={() => {setCount(count-1)}}>Decrease</button>
+      <button onClick={() => {setCount(0)}}>Set to Zero</button>
+      <h1>{count}</h1>
     </div>
   );
 }
