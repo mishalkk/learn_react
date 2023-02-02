a4138c60629ee485551a17e4d94322c376ca4b43
mishalkk3 <mishal.kk@simelabs.com>
2023-02-02 16:09:08 +0530

    complete button in Todo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import { Task } from './Task';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -13,6 +14,7 @@ function App() {
     const task = {
       id: todoList.length === 0 ? 1 : todoList[todoList.length - 1].id +1,
       taskName: newTask,
+      completed: false
     }
 
     setTodoList([...todoList, task]);
@@ -23,7 +25,20 @@ function App() {
     const newTodoList =todoList.filter((task) => task.id !== id);
 
     setTodoList(newTodoList);
-  }
+  };
+
+  const completeTask = (id) => {
+    setTodoList(
+      todoList.map((task) => {
+        if (task.id === id) {
+          // ...task keep all other parameter the same
+          return {...task, completed: true}
+        } else {
+          return task;
+        }
+      })
+    )
+  };
 
   return (
     <div className="App">
@@ -34,10 +49,12 @@ function App() {
       <div className='list'>
         {todoList.map((task) => {
           return (
-            <div>
-              <h1>{task.taskName}</h1>
-              <button onClick={() => deleteTask(task.id)}>X</button>
-            </div>
+            <Task 
+            taskName={task.taskName} 
+            id={task.id} 
+            deleteTask={deleteTask}
+            completed={task.completed}  
+            completeTask={completeTask} />
           );
         })}
       </div>
