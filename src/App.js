7e6c521e1fac4eb7a0b2d775d0aeeb7c5978e331
mishalkk3 <mishal.kk@simelabs.com>
2023-02-17 12:50:16 +0530

    API using Fetch

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import './App.css';
 
 function App() {
 
+  fetch("https://catfact.ninja/fact")
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data)
+  });
+
   return (
     <div className="App">
       <button >Genarate Cat Fact</button>
