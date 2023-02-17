8eb3323e125da3577cb7d4169e91222404e8ce23
mishalkk3 <mishal.kk@simelabs.com>
2023-02-17 12:44:24 +0530

    module 7

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import { useState } from 'react';
-import { Text } from './Text';
 
 function App() {
-  const [showText, setShowText] = useState(false);
 
   return (
     <div className="App">
-      <button onClick={() => {setShowText(!showText)}}>Show Text</button>
-      {showText && <Text />}
+      <button >Genarate Cat Fact</button>
+      <p></p>
     </div>
   );
 }
