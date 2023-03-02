63fe76f4c2f09b0984326bb9492da00f71c83bb2
mishalkk3 <mishal.kk@simelabs.com>
2023-03-02 12:55:22 +0530

    Module 11

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
-import { Home } from './pages/Home';
-import { Profile } from './pages/Profile';
-import { Contact } from './pages/Contact';
-import { NoPage } from './pages/NoPage';
-import { Navbar } from './Navbar';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
+import { Form } from './components/Form';
 
 // install react router dom
   // npm install react-router-dom
@@ -14,29 +7,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
 
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        // do not reload page when switching windows
-        refetchOnWindowFocus: false,
-      }
-    }
-  });
-
   return (
     <div className="App">
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/contact" element={<Contact />} />
-            
-            <Route path="*" element={<NoPage />} />
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+      <Form />        
     </div>
   );
 }
