488fe9e2d56331f4e0a6aaf10c0ed5f8d8ac1265
mishalkk3 <mishal.kk@simelabs.com>
2023-03-03 15:10:35 +0530

    module 13

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import './App.css';
-import { useToggle } from './useToggle';
 
 function App() {
 
-  const [isVisible, toggle] = useToggle();
-  // reusable
-  const [isVisible1, toggle1] = useToggle();
-
   return (
     <div className="App">
-      <button onClick={toggle}>{isVisible ? "Hide":"Show"}</button>
-      {isVisible && <h1>Hidden Text</h1>}
     </div>
   );
 }
