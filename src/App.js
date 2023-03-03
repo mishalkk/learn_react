cead204f9c23dd60358ed87e13fb3d7d95ca9ef8
mishalkk3 <mishal.kk@simelabs.com>
2023-03-03 15:49:46 +0530

    type safety

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import { Person } from './components/Person';
 
 function App() {
 
   return (
     <div className="App">
+      <Person name="Morty" email="morty@gmail.com" age={16} isMarried={false} 
+      friends={["Rick", "Summer", "Jerry", "Beth"]} />
     </div>
   );
 }
