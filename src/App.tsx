953a88412bd7500b4979d4f87bae236de2b075bc
mishalkk3 <mishal.kk@simelabs.com>
2023-03-03 17:16:43 +0530

    module 14

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import React, { useState } from 'react';
 import './App.css';
-import { Country, Person } from './components/Person';
+
 
 function App() {
-  // const [name, setName] = useState<string>("");
 
   return (
     <div className="App">
-      <Person name="Morty" email="morty@gmail.com" age={16} isMarried={false} 
-      friends={["Rick", "Jerry", "Summer", "Beth"]} 
-      country={Country.Brazil} />
+      
     </div>
   );
 }
