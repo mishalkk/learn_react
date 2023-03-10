b784cd6409ea1b530b63bc72cc47bea552dab7d8
mishalkk3 <mishal.kk@simelabs.com>
2023-03-10 16:41:15 +0530

    module 15 - Firebase project

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { Provider } from 'react-redux';
-import { store } from './store';
-import { Login } from './pages/Login';
-import { Contact } from './pages/Contact';
-import { Home } from './pages/Home';
-
 
 function App() {
-
   return (
     <div className="App">
-      <Provider store={store}>
-        <Router>
-          <Link to="/">Home</Link>
-          <Link to="/login">Login</Link>
-          <Link to="/contact">Contact</Link>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/contact' element={<Contact />} />
-          </Routes>
-        </Router>
-      </Provider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
--- src/pages/Home.tsx
+++
@@ -1,6 +0,0 @@
-import { useSelector } from "react-redux"
-
-export const Home = () => {
-    const username = useSelector((state: any) => state.user.value.username);
-    return <h1>THIS IS HOME PAGE - {username}</h1>
-}
